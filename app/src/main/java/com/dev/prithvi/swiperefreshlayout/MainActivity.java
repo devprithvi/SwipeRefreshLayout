515d9e54357ab63bca1f0c4417b8ec0f651cbb00
devprithvi <kprithvi26@gmail.com>
2021-10-01 16:07:25 +0530

    Initial commit

---
+++ app/src/main/java/com/dev/prithvi/swiperefreshlayout/MainActivity.java
@@ -0,0 +1,52 @@
+package com.dev.prithvi.swiperefreshlayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+    ArrayList arrayList = new ArrayList();
+    SwipeRefreshLayout swipeRefreshLayout;
+    ListView mListView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        swipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipeRefreshLayout);
+        mListView = (ListView) findViewById(R.id.listViewItem);
+
+        swipeRefreshLayout.setColorSchemeResources(R.color.purple_700);
+        arrayList.add("First Element");
+        arrayList.add("Second Element");
+        arrayList.add("Third Element");
+        arrayList.add("Fourth Element");
+        arrayList.add("Fifth Element");
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, arrayList);
+        mListView.setAdapter(adapter);
+
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                shuffle();
+                swipeRefreshLayout.setRefreshing(false);
+            }
+        });
+
+    }
+
+    private void shuffle() {
+        Collections.shuffle(arrayList, new Random(System.currentTimeMillis()));
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, arrayList);
+        mListView.setAdapter(adapter);
+    }
+}
